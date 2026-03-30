215a3bcf71f6cccdf35991608c4881094e0878c0
abdullahalrashed <raashare1@gmail.com>
2026-03-30 14:49:34 +0600

    Arrray part 1

--- Java Fundamentals.js
+++ Java Fundamentals.js
@@ -67,3 +67,28 @@ while(required)
 console.log(required)
 required= false
 }
+
+var marks = Array(6)
+var marks = new Array(20,40,35,12,37,100)
+
+var marks =[20,40,35,12,37,100]
+subMarks =marks.slice(2,5)
+console.log(subMarks)
+
+console.log(marks[2]) //35
+marks[3] = 14
+console.log(marks) //[20,40,35,14,37,100]
+console.log(marks.length) //6
+marks.push(65)
+// push adds the element at the end of the array
+console.log(marks) //[20,40,35,14,37,100,65]
+marks.pop()
+//[20,40,35,14,37,100]
+//pop removes the last element of the array
+
+marks.unshift(12)
+// unshift adds the element at the beginning of the array
+console.log(marks)
+console.log(marks.indexOf(100))
+//120 in the array
+console.log(marks.includes(120))
