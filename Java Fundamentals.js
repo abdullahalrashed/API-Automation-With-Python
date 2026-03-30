f473ee4825400e47a1cb878bf7db5d1cde2707bd
abdullahalrashed <raashare1@gmail.com>
2026-03-30 11:52:55 +0600

    Java Fundamentals

---
+++ Java Fundamentals.js
@@ -0,0 +1,38 @@
+console.log("Hello Fucker")
+
+let a=4
+console.log(a)
+console.log(typeof(a))
+
+let b = 234.345
+console.log(typeof(b))
+
+var c = "Rashed"
+console.log(typeof(c))
+
+let required = true
+console.log(typeof(required))
+
+//null and undefined
+
+// let c = a+b ( it did not work //we cannot redeclare variable with let keyword but possible with var)
+
+// reassigning is allowed with let
+c = a+b 
+//var c=a+b )this is also allowed)
+console.log(c)
+
+console.log(!required)
+
+const flag = true
+
+if(flag)
+{
+    console.log("condition satisfied")
+}
+else
+{
+    console.log("condition not satisfied")
+}
+
+let i=0
